c949fa07e6d7d54d44a327b76947f2a31c691004
om sharma <58783571+sharmaom391@users.noreply.github.com>
2022-12-03 15:13:53 +0530

    Om:add geekdemy folder

---
+++ geekdemy/geektrust.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+def main():
+    
+    """
+    Sample code to read inputs from the file
+
+    if len(argv) != 2:
+        raise Exception("File path not entered")
+    file_path = argv[1]
+    f = open(file_path, 'r')
+    Lines = f.readlines()
+    //Add your code here to process the input commands
+    """
+    
+if __name__ == "__main__":
+    main()
